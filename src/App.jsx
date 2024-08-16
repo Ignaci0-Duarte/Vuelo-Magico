0304213504d5ffd80e35a7200461bd0c4e6eb8cc
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-08-16 12:30:37 -0300

    arreglos finales

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Navbar, Container, Nav, Row, Col, Image } from "react-bootstrap";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import tela from "../public/tela2.jpeg";
+import tela from "../public/nuevatela.jpeg";
 import foto1 from "../public/foto1.jpeg";
 import foto2 from "../public/foto2.jpeg";
 import foto3 from "../public/foto3.jpeg";
@@ -14,7 +14,10 @@ import foto5 from "../public/foto5.jpeg";
 import foto6 from "../public/foto6.jpeg";
 import foto7 from "../public/foto7.jpeg";
 import foto8 from "../public/foto8.jpeg";
-import foto9 from "../public/foto9.jpeg";
+import foto9 from "../public/nuevas4.jpeg";
+import nueva1 from "../public/nuevas1.jpeg";
+import nueva2 from "../public/nuevas2.jpeg";
+import nueva3 from "../public/nuevas3.jpeg";
 import profe1 from "../public/profe1.jpeg";
 import profe2 from "../public/profe2.jpeg";
 import profe3 from "../public/profe3.jpeg";
@@ -71,13 +74,13 @@ const App = () => {
           <Col xs={6} className="text-center py-4">
             <h1
               style={{ color: "#1D687B", textAlign: "center" }}
-              className="tituloG"
+              className="tituloGB"
             >
               VUELO
             </h1>
             <h1
               style={{ color: "#16C8AA", textAlign: "center" }}
-              className="mb-5 tituloG"
+              className="mb-5 tituloGB"
             >
               MÁGICO
             </h1>
@@ -108,8 +111,6 @@ const App = () => {
           modules={[Autoplay]}
           spaceBetween={0}
           slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
         >
           <SwiperSlide>
             <img src={foto1} className="img-fluid w-100 h-40" />
@@ -118,19 +119,28 @@ const App = () => {
             <img src={foto2} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={foto3} className="img-fluid w-100 h-40" />
+            <img src={foto4} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={foto4} className="img-fluid w-100 h-40" />
+            <img src={foto3} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
             <img src={foto5} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
+            <img src={nueva3} className="img-fluid w-100 h-40" />
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={foto6} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
+            <img src={nueva2} className="img-fluid w-100 h-40" />
+          </SwiperSlide>
+          <SwiperSlide>
             <img src={foto7} className="img-fluid w-100 h-40" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={nueva1} className="img-fluid w-100 h-40" />
           </SwiperSlide>
           <SwiperSlide>
             <img src={foto8} className="img-fluid w-100 h-40" />
@@ -138,9 +148,12 @@ const App = () => {
           <SwiperSlide>
             <img src={foto9} className="img-fluid w-100 h-40" />
           </SwiperSlide>
+          <SwiperSlide>
+            <img src={nueva1} className="img-fluid w-100 h-40" />
+          </SwiperSlide>
         </Swiper>
 
-        <h2 className="text-center my-5 tituloG" style={{ color: "#1D687B" }}>
+        <h2 className="text-center my-5 tituloGP" style={{ color: "#1D687B" }}>
           NUESTRAS PROFESORAS
         </h2>
 
@@ -169,31 +182,6 @@ const App = () => {
           </Col>
           <Col xs={12} md={4} className="mb-4">
             <img
-              src={profe2}
-              alt={`Profesor`}
-              className="rounded-circle"
-              style={{ width: "250px", height: "250px" }}
-            />
-            <h3 style={{ color: "#1D687B" }} className="titulo">
-              Ana Carolina Lazarte
-            </h3>
-            <h4 style={{ color: "#16C8AA" }} className="subtitulo">
-              Acrobacia Aérea
-            </h4>
-            <p style={{ color: "#1D687B" }} className="mx-2">
-              La mayoría me conoce como Cocó. Llevo 15 años dedicada a las
-              Acrobacias Aéreas, una disciplina que me permite volar, literal y
-              figurativamente. En estos años me formé como artista y profesora,
-              trabajando duro para perfeccionar mis habilidades, combinando
-              gracia, fuerza y una profunda pasión por lo que hago. Cada vez que
-              estoy en el aire, siento que estoy en mi elemento. Y esto lo
-              quiero compartir con mis alumnos, la experiencia de sentirse
-              libres, fuertes y felices. En este retiro te invito a descubrir la
-              Magia de Volar conmigo.
-            </p>
-          </Col>
-          <Col xs={12} md={4} className="mb-4">
-            <img
               src={profe3}
               alt={`Profesor`}
               className="rounded-circle"
@@ -215,6 +203,31 @@ const App = () => {
               y conocimientos sobre la práctica del yoga. Durante este retiro,
               te invitaré al baile de tu cuerpo y tu alma al ritmo de la
               respiración y los movimientos de las asanas.
+            </p>
+          </Col>
+          <Col xs={12} md={4} className="mb-4">
+            <img
+              src={profe2}
+              alt={`Profesor`}
+              className="rounded-circle"
+              style={{ width: "250px", height: "250px" }}
+            />
+            <h3 style={{ color: "#1D687B" }} className="titulo">
+              Ana Carolina Lazarte
+            </h3>
+            <h4 style={{ color: "#16C8AA" }} className="subtitulo">
+              Acrobacia Aérea
+            </h4>
+            <p style={{ color: "#1D687B" }} className="mx-2">
+              Argentina, viviendo en España, Directora general. La mayoría me
+              conoce como Cocó. Llevo 15 años dedicada a las Acrobacias Aéreas,
+              una disciplina que me permite volar, literal y figurativamente. En
+              estos años me formé como artista y profesora, trabajando duro para
+              perfeccionar mis habilidades, combinando gracia, fuerza y una
+              profunda pasión por lo que hago. Cada vez que estoy en el aire,
+              siento que estoy en mi elemento. Y esto lo quiero compartir con
+              mis alumnos, la experiencia de sentirse libres, fuertes y felices.
+              En este retiro te invito a descubrir la Magia de Volar conmigo.
             </p>
           </Col>
         </Row>
